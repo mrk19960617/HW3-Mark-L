44a4376d25f39afdd0d5a83502edb795285746b3
mrk19960617 Lienhard <mark.lienhard@kellogg.northwestern.edu>
2025-02-23 22:09:29 +0000

    Models

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,4 @@
 class Entry < ApplicationRecord
-  # talks to the "entries" table
+  belongs_to :place   # Each entry belongs to one place
+  validates :title, :description, :date, presence: true  # Ensures required fields
 end
