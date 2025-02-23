b3e47a049870415670eef443663b9703a137020c
mrk19960617 Lienhard <mark.lienhard@kellogg.northwestern.edu>
2025-02-23 22:04:08 +0000

    Commit places

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'places/index'
+  get 'places/show'
+  get 'places/new'
+  get 'places/create'
+  # Define routes for Places
+  get("/places", { :controller => "places", :action => "index" })   # List all places
+  get("/places/new", { :controller => "places", :action => "new" }) # Show form for a new place
+  post("/places", { :controller => "places", :action => "create" }) # Create a new place
+  get("/places/:id", { :controller => "places", :action => "show" }) # Show a specific place
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  # Define routes for Entries (Nested under Places)
+  get("/places/:place_id/entries/new", { :controller => "entries", :action => "new" })  # Form to add entry
+  post("/places/:place_id/entries", { :controller => "entries", :action => "create" })  # Create entry
+
+  # Set root route to show the list of places
+  root "places#index"
 end
